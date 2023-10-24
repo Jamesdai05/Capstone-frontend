6c3a96cafe2eeb547b01ba36946115b43c30e5df
jamesdai05 <dai_guilong@hotmail.com>
2023-10-24 14:29:01 +0800

    add the usersSlices

---
+++ frontend/elog-client/src/redux/slices/users/usersSlices.js
@@ -0,0 +1,56 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const registrationAction = createAsyncThunk(
+  "auth/register",
+  async (user, { rejectWithValue, getState, dispatch }) => {
+    try {
+      // http call
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const data = await axios.post(
+        "localhost:8080/auth/register",
+        user,
+        config
+      );
+      return data;
+    } catch (error) {
+      // if(!error && ! error.response)
+      if (!error?.response) {
+        throw error;
+      }
+      return rejectWithValue(error?.response?.data);
+    }
+  }
+);
+
+const usersSlices = createSlice({
+  name: "users",
+  initialState: {
+    usersAuth: "login",
+  },
+
+  extraReducers: {
+    [registrationAction.pending]: (state, action) => {
+      state.loading = true;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    },
+    [registrationAction.fulfilled]: (state, action) => {
+      state.registered = action.payload;
+      state.appErr = undefined;
+      state.serverErr = undefined;
+    },
+
+    [registrationAction.rejected]: (state, action) => {
+      state.registered = false;
+      state.appErr = action?.payload?.message;
+      state.serverErr = action?.error?.message;
+    },
+  },
+});
+
+export default usersSlices.reducer;
