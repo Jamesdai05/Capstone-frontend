b000174d542c920975cd9f143963d096ad64db11
jamesdai05 <dai_guilong@hotmail.com>
2023-10-25 16:22:53 +0800

    add image to the header

--- frontend/elog-client/src/components/Header/Header.jsx
+++ frontend/elog-client/src/components/Header/Header.jsx
@@ -9,6 +9,7 @@ export default function Header() {
         <span className="titleLg">E-log</span>
         <img
           src="https://fastly.picsum.photos/id/416/900/400.jpg?hmac=2SV5t47LKCjdINJFhG8RPmwrOQIAnroN8W23ZWnV8RY"
+          // src="https://images.pexels.com/photos/6476808/pexels-photo-6476808.jpeg?auto=compress&cs=tinysrgb&w=1600"
           alt="img"
         />
       </div>
