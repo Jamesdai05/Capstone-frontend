2d63bceeafd3a599c8b2a7d718549498641da91b
jamesdai05 <dai_guilong@hotmail.com>
2023-10-26 04:33:40 +0800

    style the form

--- frontend/elog-client/src/Pages/reportCreate/Create.jsx
+++ frontend/elog-client/src/Pages/reportCreate/Create.jsx
@@ -3,39 +3,48 @@ import "./report.css";
 
 export default function CreateReport() {
   return (
-    <div>
-      <h1>Report creation</h1>
-      <form action="">
-        <div className="creat-title">
-          <label htmlFor="Title">Title:</label>
-          <br></br>
-          <input
-            className="createReport"
-            type="text"
-            placeholder="Enter the title here"
-          />
-        </div>
-        <div>
-          <label htmlFor="cat">Category:</label>
-          <br></br>
-          <input type="text" className="createReport" placeholder="category" />
-        </div>
-        <div className="form">
-          <label htmlFor="description">Description:</label>
-          <br></br>
-          <textarea
-            className="createReport"
-            type="text"
-            row="30"
-            column="10"
-            placeholder="Enter the content here"
-          />
-        </div>
-
-        <button type="button" class="btn btn-primary">
-          Submit
-        </button>
-      </form>
-    </div>
+    <>
+      <div>
+        <h1>Report creation</h1>
+      </div>
+      <div>
+        <form action="">
+          <div className="creat-title">
+            <label htmlFor="Title">Title:</label>
+            <br></br>
+            <input
+              className="createReport"
+              type="text"
+              placeholder="Enter the title here"
+            />
+          </div>
+          <div>
+            <label htmlFor="cat">Category:</label>
+            <br></br>
+            <input
+              type="text"
+              className="createReport"
+              placeholder="category"
+            />
+          </div>
+          <div className="form">
+            <label htmlFor="description">Description:</label>
+            <br></br>
+            <textarea
+              className="createReport"
+              type="text"
+              rows="15"
+              cols="15"
+              placeholder="Enter the content here"
+            />
+          </div>
+          <div>
+            <button type="button" class="btn btn-primary">
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </>
   );
 }
