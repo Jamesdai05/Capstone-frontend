87f16d853e1752229959ee117a67c4bb06079032
jamesdai05 <dai_guilong@hotmail.com>
2023-10-26 03:17:53 +0800

    add the create form

--- frontend/elog-client/src/Pages/reportCreate/Create.jsx
+++ frontend/elog-client/src/Pages/reportCreate/Create.jsx
@@ -4,7 +4,14 @@ import classes from "./report.module.css";
 export default function CreateReport() {
   return (
     <div>
-      <h1>This is report creation page</h1>
+      <h1>Report creation</h1>
+      <form action="">
+        <label htmlFor="Title">Title</label>
+        <input type="text" />
+        <label htmlFor="description">Description</label>
+        <textarea type="text" row="30" column="10" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
