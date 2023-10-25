55e5f06d99cb7341dd3a8ec795d6a9809d51be43
jamesdai05 <dai_guilong@hotmail.com>
2023-10-25 15:49:15 +0800

    import the header css

--- frontend/elog-client/src/components/Header/Header.jsx
+++ frontend/elog-client/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import "./Header.css";
 
 export default function Header() {
   return (
-    <div className="Header">
-      <div className="headertitle">
-        {/* <div className="headerTitleLg">E-log</div> */}
-        {/* <img
+    <div className="header">
+      <div className="title">
+        <div className="titleLg">E-log</div>
+        <img
           src="https://fastly.picsum.photos/id/416/900/400.jpg?hmac=2SV5t47LKCjdINJFhG8RPmwrOQIAnroN8W23ZWnV8RY"
           alt="img"
-        /> */}
+        />
       </div>
     </div>
   );
