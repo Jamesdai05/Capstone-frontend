9f2f662b9a63adc13b3a0a4d56f033dda55ab32c
jamesdai05 <dai_guilong@hotmail.com>
2023-10-25 16:11:29 +0800

    add the header style

--- frontend/elog-client/src/Pages/Home/Homepage.jsx
+++ frontend/elog-client/src/Pages/Home/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import "./Home.css";
 import { Footer } from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
 
 export default function Home() {
   return (
     <div>
+      <Header />
       <h1 id="hometitle">Welcome to E-log</h1>
       <Footer />
     </div>
--- frontend/elog-client/src/components/Header/Header.jsx
+++ frontend/elog-client/src/components/Header/Header.jsx
@@ -5,7 +5,8 @@ export default function Header() {
   return (
     <div className="header">
       <div className="title">
-        <div className="titleLg">E-log</div>
+        <span className="titleSm">Welcome To</span>
+        <span className="titleLg">E-log</span>
         <img
           src="https://fastly.picsum.photos/id/416/900/400.jpg?hmac=2SV5t47LKCjdINJFhG8RPmwrOQIAnroN8W23ZWnV8RY"
           alt="img"
