7b8abed504920aa176f03c1d93f2f775a83f21c5
jamesdai05 <dai_guilong@hotmail.com>
2023-10-26 04:09:34 +0800

    add the form to the create page

--- frontend/elog-client/src/Pages/reportCreate/Create.jsx
+++ frontend/elog-client/src/Pages/reportCreate/Create.jsx
@@ -1,16 +1,40 @@
 import React from "react";
-import classes from "./report.module.css";
+import "./report.css";
 
 export default function CreateReport() {
   return (
     <div>
       <h1>Report creation</h1>
       <form action="">
-        <label htmlFor="Title">Title</label>
-        <input type="text" />
-        <label htmlFor="description">Description</label>
-        <textarea type="text" row="30" column="10" />
-        <button type="submit">Submit</button>
+        <div className="creat-title">
+          <label htmlFor="Title">Title:</label>
+          <br></br>
+          <input
+            className="createReport"
+            type="text"
+            placeholder="Enter the title here"
+          />
+        </div>
+        <div>
+          <label htmlFor="cat">Category:</label>
+          <br></br>
+          <input type="text" className="createReport" placeholder="category" />
+        </div>
+        <div className="form">
+          <label htmlFor="description">Description:</label>
+          <br></br>
+          <textarea
+            className="createReport"
+            type="text"
+            row="30"
+            column="10"
+            placeholder="Enter the content here"
+          />
+        </div>
+
+        <button type="button" class="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
