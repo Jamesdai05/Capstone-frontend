f5b5d94758ebb2f4d6947c2f2b52168fb185823c
jamesdai05 <dai_guilong@hotmail.com>
2023-10-31 02:29:28 +0800

    set the max-width for the postdetail

--- frontend/elog-client/src/components/Post/Postdetails/Postdetail.jsx
+++ frontend/elog-client/src/components/Post/Postdetails/Postdetail.jsx
@@ -83,7 +83,9 @@ export default function Postdetail() {
 
   return (
     <div className="showContainer">
-      <h1 className="reportTitle">{report.title}</h1>
+      <div className="showreportTitle">
+        <h1 className="reportTitle">{report.title}</h1>
+      </div>
       <div className="show-sub">
         <div>
           <p className="postBy">
@@ -97,7 +99,44 @@ export default function Postdetail() {
       <div>
         <div className="show-Main">
           <p>{report.description}</p>
-          {/* this the report paragraph. */}
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere aut
+            libero fugiat unde, repudiandae suscipit labore sapiente, magni,
+            harum accusantium consequatur? Atque fugit non ipsam magni quasi!
+            Iusto sapiente voluptas vero? Illum quasi omnis minus, commodi,
+            architecto doloribus voluptatum beatae quibusdam sequi a deserunt
+            voluptatem debitis porro eos eum expedita animi dicta? Voluptatem
+            assumenda minima cupiditate corrupti cum, reprehenderit labore
+            adipisci recusandae perspiciatis autem! Tempore quae veritatis
+            quidem veniam quaerat. Optio earum sint error nostrum dolore
+            dignissimos, laborum cumque. Iure, debitis aspernatur eligendi
+            mollitia vitae magni aliquid? Blanditiis eveniet aliquam vel
+            necessitatibus dolore ipsum debitis consequuntur, animi modi
+            laboriosam laudantium nulla non eos iste perspiciatis corporis
+            fugiat exercitationem deserunt officia. Eum, temporibus natus. In
+            similique placeat quam tempore harum consectetur officiis quis
+            deserunt facere? Id hic, reiciendis ex velit suscipit deserunt quia
+            ipsam quis atque quo aliquid magnam excepturi, dolor, ea dolores
+            quidem delectus. Doloribus blanditiis, laudantium voluptate unde
+            optio quaerat! Quo voluptates tenetur praesentium maxime, facilis
+            optio corporis animi rem unde molestias voluptatibus id earum
+            maiores laborum nesciunt iure minus, perspiciatis iusto tempora
+            itaque ipsum vitae? Similique numquam ratione nesciunt tenetur
+            nostrum. Earum adipisci nisi asperiores quibusdam ex autem ipsum
+            optio reiciendis in vitae! Praesentium facilis debitis laudantium
+            doloremque sapiente suscipit tenetur nobis, natus expedita adipisci
+            impedit rem dolorum corrupti corporis, saepe ipsum porro molestias!
+            Esse modi vitae dignissimos maiores recusandae inventore nam iste,
+            voluptatibus dolor consectetur officiis doloribus, nostrum quis
+            porro est laborum debitis nesciunt id eum pariatur, aspernatur
+            commodi eaque nobis dicta? Voluptatum cupiditate facilis quae
+            exercitationem, iure distinctio iusto pariatur deleniti harum est
+            velit voluptas expedita quibusdam voluptatibus laudantium. Amet
+            numquam laborum rem ipsum praesentium repudiandae earum dolores
+            facilis libero tempora veritatis, ipsa, error est quam sit minima
+            expedita non iusto soluta quibusdam eius voluptatem. Illo aperiam
+            ullam dolore possimus facere magni ipsa neque aliquid suscipit.
+          </p>
         </div>
         <div id="button">
           <div>
