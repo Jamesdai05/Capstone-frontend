9d64b7b393cd171c5a978cc8485f186256da7b60
jamesdai05 <dai_guilong@hotmail.com>
2023-10-31 21:30:29 +0800

    add the function for sorting

--- frontend/elog-client/src/components/Header/Header.jsx
+++ frontend/elog-client/src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import "./Header.css";
 export default function Header() {
   return (
     <div className="header">
-      <h1 className="title">Welcome To E-log</h1>
+      <h1 className="title">Welcome To E-Log</h1>
       {/* <img
           src="//images.pexels.com/photos/1167355/pexels-photo-1167355.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
           alt="img"
--- frontend/elog-client/src/components/admin/Admin.jsx
+++ frontend/elog-client/src/components/admin/Admin.jsx
@@ -2,37 +2,100 @@ import React, { useEffect, useState } from "react";
 import postSlices from "../../redux/slices/reports/postSlices";
 import axios from "axios";
 import { baseURL } from "../../utils/baseUrl";
-
+import { setNestedObjectValues } from "formik";
 
 export default function Admin() {
-  const [sortType, setSortType]=useState("ascending");
-  const [result, setResult] = useState();
-  const [report, setReport] = useState([])
+  //store store sortedBy order
+  const [sortType, setSortType] = useState("ascending");
+
+  //store field title or description
+  const [sortByField, setSortByField] = useState("title");
+
+  const [results, setResults] = useState();
+
+  //get the data and store to reports.
+  const [reports, setReports] = useState({});
+
+  const [page, setPage] = useState({
+    query: "",
+    list: reports,
+  });
 
   // console.log("test")
 
-  const makeApiCall=()=>{
-    fetch(`${baseURL}/api/posts`)
-     .then((res)=>res.json())
-     .then((data) => {
-          console.log(data);
-          setReport(data);
-        });
+  const makeApiCall = async () => {
+    let response = await fetch(`${baseURL}/api/posts`);
+    let data = await response.json();
+    console.log(data);
+    setReports(data);
+    // console.log(reports);
+  };
+
+  useEffect(() => {
+    makeApiCall();
+  }, []);
+
+  const handleChange = (e) => {
+    const results = reports.filter((element) => {
+      if (e.target.value === "") return reports;
+      return reports[sortByField].includes(e.target.value.toLowerCas());
+    });
+    setResults(results);
+    setNestedObjectValues({
+      query: e.target.value,
+      list: sortFunction(results, sortType, sortByField),
+    });
+  };
+
+  const sortFunction = (results, sortType, sortByField) => {
+    if (sortType === "asending") {
+      results.sort((a, b) => (a[sortByField] < b[sortByField] ? -1 : 1));
+    } else if (sortType === "desending") {
+      results.sort((a, b) => (a[sortByField] > b[sortByField] ? 1 : -1));
     }
-  // // make an api call to query the data.
-  // useEffect(
-  //   ()=>{
-  //     makeApiCall();
-  // },[])
+    return results;
+  };
 
+  // Drop down to sort reports in asending or descending on title.
+  const updateReports = (e) => {
+    setSortType(e);
+    setPage({
+      query: page.query,
+      list: !results
+        ? sortFunction(reports, e, sortByField)
+        : sortFunction(reports, e, sortByField),
+    });
+  };
 
-
-
+  const sortBy = (e) => {
+    setSortByField(e);
+    setPage({
+      query: page.query,
+      list: !results
+        ? sortFunction(reports, e, sortType)
+        : sortFunction(results, e, sortType),
+    });
+  };
 
   return (
     <div>
       <h1>Admin Page</h1>
-      <p>This is an Admin Page. It can be acessed only by admin/managers!</p>
+      <form action="">
+        <span>Search</span>
+        <input className="admininput" type="search" value={page.query} />
+        <span>SortBy:</span>
+        <select
+          defaultValue={"title"}
+          onChange={(e) => sortBy(e.target.value)}
+          id=""
+        >
+          <option value="DEFAULT" disabled>
+            None
+          </option>
+          <option value="ascending">Ascending</option>
+          <option value="descending">Descending</option>
+        </select>
+      </form>
     </div>
   );
 }
