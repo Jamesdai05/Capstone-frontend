45e3310e64fe5b102e22d42edb4528fee3854dd0
jamesdai05 <dai_guilong@hotmail.com>
2023-11-01 01:38:44 +0800

    add the sorting functions to homepage

--- frontend/elog-client/src/Pages/Home/Homepage.jsx
+++ frontend/elog-client/src/Pages/Home/Homepage.jsx
@@ -12,23 +12,75 @@ import { useSelector } from "react-redux";
 
 export default function Home() {
   const [reports, setReports] = useState([]);
+  const [sortType, setSortType] = useState("title");
+  const [sortByField, setSortByField] = useState("title");
+  const [result, setResult] = useState();
+  const [display, setDisplay] = useState({
+    query: "",
+    list: reports,
+  });
 
   const store = useSelector((state) => state?.users);
   const { usersAuth } = store;
   // const navigate = useNavigate();
 
-  const makeApiCall = () => {
-    fetch(`${baseURL}/api/posts`)
-      .then((res) => res.json())
-      .then((data) => {
-        // console.log(data);
-        setReports(data);
-      });
+  const makeApiCall = async () => {
+    let response = fetch(`${baseURL}/api/posts`);
+    let data = await response.json();
+    console.log(data);
+    setReports(data);
   };
 
   useEffect(() => {
     makeApiCall();
   }, []);
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.value === "") return reports;
+    const results = reports.filter((report) => {
+      if (e.target.value === "") return reports;
+      //set the sortByField for the non-null
+      return report[sortByField]
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+    });
+    setResult(results);
+
+    setDisplay({
+      query: e.target.value,
+      list: sortFunction(results, sortType, sortByField),
+    });
+  };
+
+  function sortFunction(results, sortType, sortByField) {
+    if (sortType === "ascending") {
+      results.sort((a, b) => (a[sortByField] < b[sortByField] ? -1 : 1));
+    } else if (sortType === "descending") {
+      results.sort((a, b) => (b[sortByField] > a[sortByField] ? 1 : -1));
+    }
+    return results;
+  }
+
+  function updateReports(e) {
+    setSortType(e);
+    setDisplay({
+      query: display.query, //page.query
+      list: !result
+        ? sortFunction(reports, e, sortByField)
+        : sortFunction(reports, e, sortByField),
+    });
+  }
+
+  function sortBy(e) {
+    setSortByField(e);
+    setDisplay({
+      query: display.value,
+      list: !result
+        ? sortFunction(reports, sortType, e)
+        : sortFunction(result, sortType, e),
+    });
+  }
 
   const posts = reports.map((element, index) => (
     <div className="grid-card">
@@ -43,14 +95,6 @@ export default function Home() {
       />
     </div>
   ));
-  // console.log(posts);
-  // axios
-  //   .get(`${baseURL}/api/posts`, {
-  //     responseType: "json",
-  //   })
-  //   .then(function (response) {
-  //     console.log(response.data);
-  //   });
 
   return (
     <div>
